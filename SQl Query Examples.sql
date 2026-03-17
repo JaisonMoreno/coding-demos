39c97277cd7aaf38f61f697c821c76bd5205aa7f
Jaison Moreno <47932726+JaisonMoreno@users.noreply.github.com>
2026-03-16 22:45:52 -0400

    Add files via upload

--- SQl Query Examples.sql
+++ SQl Query Examples.sql
@@ -1265,3 +1265,12 @@ group by p.title, p.budget
 having p.budget < ceiling(sum((datediff(day, start_date, end_date)/365.0) * e.salary))
 
 
+/* 39. Find the email activity rank for each user. Email activity rank is defined by the total number of emails sent. The user with the highest number of emails sent will have a rank of 1, and so on. Output the user, total emails, and their activity rank.
+•	Order records first by the total emails in descending order.
+•	Then, sort users with the same number of emails in alphabetical order by their username.
+•	In your rankings, return a unique value (i.e., a unique rank) even if multiple users have the same number of emails. */
+select from_user, count(*) as emails_sent,
+row_number() over(order by count(*) desc, from_user) as rn
+from google_gmail_emails
+group by from_user
+order by emails_sent desc, from_user
